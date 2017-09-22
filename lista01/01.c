16c60890c5ce634836410a94297149a3ea4409d3
ufabc <marco.soares.araujo@gmail.com>
2017-09-22 09:57:01 -0300

    exercício 01

---
+++ lista01/01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+	int n;
+	scanf("Entre com um valor inteiro: %d", &n);
+	if (n%2 == 0)
+	{
+		printf("par\n"); 
+	}
+	else
+	{
+		printf("ímpar\n");
+	} 
+	return 0;
+}
